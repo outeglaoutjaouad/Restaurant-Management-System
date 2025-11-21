bc19843c82e7efd2cb7d9e969fca6ee61420f9e3
loubnaaktar <loubnaakt13@gmail.com>
2025-11-21 10:24:41 +0100

    merging client commit with master

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Client client1 = new Client(1, "jaouad");
 
+        Commande cmd1 = new Commande(101, new Date(), 120.5, client1);
+        Commande cmd2 = new Commande(102, new Date(), 89.0, client1);
+
+        client1.ajouterCommande(cmd1);
+        client1.ajouterCommande(cmd2);
+
+        client1.afficherCommandes();
     }
 }
