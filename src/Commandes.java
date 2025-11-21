acd725a482d620e61de419d2d46ca47d8be59bd9
loubnaaktar <loubnaakt13@gmail.com>
2025-11-21 09:55:18 +0100

    gestion des commandes

---
+++ src/Commandes.java
@@ -0,0 +1,45 @@
+public class Commandes {
+
+    private int Idcommande;
+    // double prixTotal;
+    // Serveur serveur;
+    // Client client;
+    // List<Plat>plats
+
+//constructeur ;
+
+    //getter and setter
+
+
+
+
+    public double calculerTotal(){
+        /*
+        for(Plat p: plats){
+        prixTotal += p.getPrix;
+
+        }
+
+
+         */
+     return 0;
+    }
+    public void afficherDetails(){
+
+        //System.out.println("la commande de monsieur :" + client.getNom + ".");
+
+         System.out.println("vous avez commander : ");
+
+        //System.out.println("cette commande est assignée au serveur :" + serveur.getNom );
+         /*
+
+        for(Plat p: plats){
+
+        system.out.println("-" + p.getNomPlat ":" + p.getPrix);
+        system.out.println("le prix total de votre commande est :" + calculerTotal() + "MAD");
+        }
+          */
+
+
+     }
+}
